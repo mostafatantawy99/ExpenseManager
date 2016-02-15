12ff327f6a48f515e293efd13759afc43392f0e2
Yehun Kim <y228kim@uwaterloo.ca>
2016-02-15 01:51:18 -0500

    Work on UI

--- app/src/main/java/com/danielkim/expensemanager/DBHelper.java
+++ app/src/main/java/com/danielkim/expensemanager/DBHelper.java
@@ -1,6 +1,8 @@
 package com.danielkim.expensemanager;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
@@ -12,9 +14,9 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "expenses.db";
     private static final int DATABASE_VERSION = 1;
 
+    //Databases for tracking expenses
     public static abstract class DBHelperItem implements BaseColumns {
         public static final String TABLE_NAME = "expenses";
-
         public static final String COL_DATE = "date";
         public static final String COL_AMOUNT = "amount";
         public static final String COL_CATEGORY = "category";
@@ -23,9 +25,9 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     public static final String COMMA_SEP = ",";
-    public static final String SQL_CREATE_ENTRIES =
+    public static final String SQL_CREATE_EXPENSES_TB =
             "CREATE TABLE " + DBHelperItem.TABLE_NAME + " (" +
-                    DBHelperItem._ID + " INTEGER PRIMARY KEY AUTOINCREMENT" + COMMA_SEP +
+                    DBHelperItem._ID + " INTEGER PRIMARY KEY " + COMMA_SEP +
                     DBHelperItem.COL_DATE + " TEXT " + COMMA_SEP +
                     DBHelperItem.COL_AMOUNT + " DOUBLE " + COMMA_SEP +
                     DBHelperItem.COL_CATEGORY + " TEXT " + COMMA_SEP +
@@ -34,17 +36,41 @@ public class DBHelper extends SQLiteOpenHelper {
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(SQL_CREATE_ENTRIES);
+        db.execSQL(SQL_CREATE_EXPENSES_TB);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + DBHelperItem.TABLE_NAME);
         onCreate(db);
+    }
+
+    //insert new transaction into the Expenses table
+    public long insertExpense(String date, double amt, String category, String method, String notes){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(DBHelperItem.COL_DATE, date);
+        cv.put(DBHelperItem.COL_AMOUNT, amt);
+        cv.put(DBHelperItem.COL_CATEGORY, category);
+        cv.put(DBHelperItem.COL_PAYMENT_METHOD, method);
+        cv.put(DBHelperItem.COL_NOTES, notes);
+
+        long rowId = db.insert(DBHelperItem.TABLE_NAME, null, cv);
+        return rowId;
+    }
+
+    // returns the rows inserted between date1 and date2
+    public Cursor getExpenseTableDataDateDescending(){
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor c = db.rawQuery("SELECT * FROM " +
+                DBHelperItem.TABLE_NAME + " ORDER BY " +
+                DBHelperItem.COL_DATE + " DESC", null);
+
+        return c;
     }
 }
